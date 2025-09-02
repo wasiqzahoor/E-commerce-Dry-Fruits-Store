857a11e52b5ee0610ba0d86e49ee6b579ebb6b65
Chaudhary Wasiq Zahoor <168441965+wasiqzahoor@users.noreply.github.com>
2025-09-02 15:44:29 +0500

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,11 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-
-    window.scrollTo(0, 0);
-  }, [pathname]); 
-
-  return null; 
-};
-
-export default ScrollToTop;import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom"; 
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
+// ... Baaqi saare page imports waise hi rahenge
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
-import Navbar from "./components/Navbar";
 import AuthPage from "./pages/AuthPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MyOrdersPage from "./pages/MyOrdersPage";
@@ -27,28 +14,18 @@ import AdminProtectedRoute from "./components/AdminProtectedRoute";
 import AdminOrderListPage from "./pages/admin/AdminOrderListPage";
 import AdminProductListPage from "./pages/admin/AdminProductListPage";
 import AdminUserListPage from "./pages/admin/AdminUserListPage";
-import AdminOrderHistoryPage from "./pages/admin/AdminOrderHistoryPage"; 
+import AdminOrderHistoryPage from "./pages/admin/AdminOrderHistoryPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SavedItemsPage from "./pages/SavedItemsPage";
 import AdminLayout from "./components/AdminLayout";
 import AdminPromotionsPage from "./pages/admin/AdminPromotionsPage";
-import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
-import ScrollToTop from "./components/ScrollToTop";
+import Layout from "./components/Layout"; // Naya Layout component import karein
 
 function App() {
-  const location = useLocation();
-  const pathsWithoutFooter = ['/auth'];
-  const isAdminRoute = location.pathname.startsWith('/admin');
-
-  const showFooter = !pathsWithoutFooter.includes(location.pathname) && !isAdminRoute;
-
   return (
     <>
-      <Navbar />
-      <ScrollToTop />
-      
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -65,35 +42,35 @@ function App() {
 
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/saved-items" element={<SavedItemsPage />} />
+          {/* Naya Layout Route banayein jo baaqi routes ko wrap karega */}
+          <Route path="/" element={<Layout />}>
+            {/* Saare non-admin routes ab iske andar aayenge */}
+            <Route index element={<HomePage />} /> {/* Homepage ke liye index route */}
+            <Route path="product/:id" element={<ProductDetailPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="auth" element={<AuthPage />} />
+            <Route path="products" element={<ProductList />} />
+            <Route path="saved-items" element={<SavedItemsPage />} />
 
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedRoute>
-                <CheckoutPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/my-orders"
-            element={
-              <ProtectedRoute>
-                <MyOrdersPage />
-              </ProtectedRoute>
-            }
-          />
+            <Route
+              path="checkout"
+              element={
+                <ProtectedRoute> <CheckoutPage /> </ProtectedRoute>
+              }
+            />
+            <Route
+              path="my-orders"
+              element={
+                <ProtectedRoute> <MyOrdersPage /> </ProtectedRoute>
+              }
+            />
+          </Route>
+          
+          {/* Admin routes ko alag rakhein */}
           <Route 
             path="/admin" 
             element={
-              <AdminProtectedRoute>
-                <AdminLayout />
-              </AdminProtectedRoute>
+              <AdminProtectedRoute> <AdminLayout /> </AdminProtectedRoute>
             }
           >
             <Route path="dashboard" element={<AdminDashboardPage />} />
@@ -105,8 +82,6 @@ function App() {
           </Route>
         </Routes>
       </main>
-
-      {showFooter && <Footer />}
     </>
   );
 }
